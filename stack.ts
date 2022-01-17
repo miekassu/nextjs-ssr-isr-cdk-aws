5c95ca2eefe0639c1479dab0bf50d4b318da4741
Kasper <kasper.hamalainen@gmail.com>
2022-01-17 21:05:39 +0700

    feat: print deployment url on terminal

--- stack.ts
+++ stack.ts
@@ -1,13 +1,14 @@
-import * as cdk from "@aws-cdk/core";
-import { Duration } from "@aws-cdk/core";
-import { NextJSLambdaEdge } from "@sls-next/cdk-construct";
-import { Runtime } from "@aws-cdk/aws-lambda";
+import * as cdk from '@aws-cdk/core';
+import { CfnOutput, Duration } from '@aws-cdk/core';
+import { NextJSLambdaEdge } from '@sls-next/cdk-construct';
+import { Runtime } from '@aws-cdk/aws-lambda';
 
 export class NextStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
-    new NextJSLambdaEdge(this, "NextJsApp", {
-      serverlessBuildOutDir: "./build",
+
+    const deployment = new NextJSLambdaEdge(this, 'NextJsApp', {
+      serverlessBuildOutDir: './build',
       runtime: Runtime.NODEJS_12_X,
       memory: 1024,
       timeout: Duration.seconds(30),
@@ -18,5 +19,10 @@ export class NextStack extends cdk.Stack {
         imageLambda: `${id}Image`,
       },
     });
+
+    new CfnOutput(this, 'DeploymentDomainName', {
+      value: deployment.distribution.domainName,
+    });
+
   }
 }
